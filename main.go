b86ba80294755803e584077a9df562e3d2dc0fa7
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2025-01-11 11:26:46 +0530

    Updated code

--- main.go
+++ main.go
@@ -7,13 +7,10 @@ import (
 	"github.com/PragaL15/go_newBackend/go_backend/db"
 	"github.com/PragaL15/go_newBackend/handlers"
 )
-
 func main() {
     db.ConnectDB()
     app := fiber.New()
-
     app.Get("/users", handlers.GetAllUsers) 
     app.Get("/user-bankdetails", handlers.GetUserBankDetails) 
     log.Fatal(app.Listen(":3000"))
-
 }
