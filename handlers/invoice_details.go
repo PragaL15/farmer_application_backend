f880db5f7a17c1a19f798020bd9b3b2c7b1fbd97
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2025-02-26 20:03:20 +0530

    updated invoice handler function

--- handlers/invoice_details.go
+++ handlers/invoice_details.go
@@ -10,34 +10,38 @@ import (
 )
 
 type Invoice struct {
-	ID                  int       `json:"id"`
-	InvoiceNumber       string    `json:"invoice_number"`
-	OrderID             int       `json:"order_id"`
-	TotalAmount         float64   `json:"total_amount"`
-	RetailerID          int       `json:"retailer_id"`
-	RetailerName        string    `json:"retailer_name"`
-	RetailerEmail       string    `json:"retailer_email"`
-	RetailerPhone       string    `json:"retailer_phone"`
-	RetailerAddress     string    `json:"retailer_address"`
-	RetailerLocationID  int       `json:"retailer_location_id"`
-	RetailerStateID     int       `json:"retailer_state_id"`
-	WholesellerID       int       `json:"wholeseller_id"`
-	WholesellerName     string    `json:"wholeseller_name"`
-	WholesellerEmail    string    `json:"wholeseller_email"`
-	WholesellerPhone    string    `json:"wholeseller_phone"`
-	WholesellerAddress  string    `json:"wholeseller_address"`
-	WholesellerLocationID int     `json:"wholeseller_location_id"`
-	WholesellerStateID  int       `json:"wholeseller_state_id"`
-	DiscountAmount      float64   `json:"discount_amount"`
-	InvoiceDate         string    `json:"invoice_date"`
-	DueDate             string    `json:"due_date"`
-	PayMode             int       `json:"pay_mode"`
-	PayModeName         string    `json:"pay_mode_name"`
-	PayType             int       `json:"pay_type"`
-	PayTypeName         string    `json:"pay_type_name"`
-	FinalAmount         float64   `json:"final_amount"`
-	StatusName          string    `json:"status_name"`
-	Products            []Product `json:"products"`
+	ID                    int       `json:"id"`
+	InvoiceNumber         string    `json:"invoice_number"`
+	OrderID               int       `json:"order_id"`
+	TotalAmount           float64   `json:"total_amount"`
+	RetailerID            int       `json:"retailer_id"`
+	RetailerName          string    `json:"retailer_name"`
+	RetailerEmail         string    `json:"retailer_email"`
+	RetailerPhone         string    `json:"retailer_phone"`
+	RetailerAddress       string    `json:"retailer_address"`
+	RetailerLocationID    int       `json:"retailer_location_id"`
+	RetailerStateID       int       `json:"retailer_state_id"`
+	RetailerStateName     string    `json:"retailer_state_name"`
+	RetailerLocationName  string    `json:"retailer_location_name"`
+	WholesellerID         int       `json:"wholeseller_id"`
+	WholesellerName       string    `json:"wholeseller_name"`
+	WholesellerEmail      string    `json:"wholeseller_email"`
+	WholesellerPhone      string    `json:"wholeseller_phone"`
+	WholesellerAddress    string    `json:"wholeseller_address"`
+	WholesellerLocationID int       `json:"wholeseller_location_id"`
+	WholesellerStateID    int       `json:"wholeseller_state_id"`
+	WholesellerStateName  string    `json:"wholeseller_state_name"`
+	WholesellerLocationName string  `json:"wholeseller_location_name"`
+	DiscountAmount        float64   `json:"discount_amount"`
+	InvoiceDate           string    `json:"invoice_date"`
+	DueDate               string    `json:"due_date"`
+	PayMode               int       `json:"pay_mode"`
+	PayModeName           string    `json:"pay_mode_name"`
+	PayType               int       `json:"pay_type"`
+	PayTypeName           string    `json:"pay_type_name"`
+	FinalAmount           float64   `json:"final_amount"`
+	StatusName            string    `json:"status_name"`
+	Products              []Product `json:"products"`
 }
 
 type Product struct {
@@ -62,14 +66,18 @@ func GetInvoiceDetails(c *fiber.Ctx) error {
 		var invoiceDate, dueDate time.Time
 
 		err := rows.Scan(
-			&invoice.ID, &invoice.InvoiceNumber, &invoice.OrderID, &invoice.RetailerID, &invoice.RetailerName,
-			&invoice.RetailerEmail, &invoice.RetailerPhone, &invoice.RetailerAddress, &invoice.RetailerLocationID,
-			&invoice.RetailerStateID, &invoice.WholesellerID, &invoice.WholesellerName, &invoice.WholesellerEmail,
-			&invoice.WholesellerPhone, &invoice.WholesellerAddress, &invoice.WholesellerLocationID,
-			&invoice.WholesellerStateID, &invoice.TotalAmount, &invoice.DiscountAmount, &invoiceDate,
-			&dueDate, &invoice.PayMode, &invoice.PayModeName, &invoice.PayType, &invoice.PayTypeName,
-			&invoice.FinalAmount, &product.OrderItemID, &product.ProductID, &product.ProductName,
-			 &invoice.StatusName,
+			&invoice.ID, &invoice.InvoiceNumber, &invoice.OrderID, 
+			&invoice.RetailerID, &invoice.RetailerName, &invoice.RetailerEmail, 
+			&invoice.RetailerPhone, &invoice.RetailerAddress, &invoice.RetailerLocationID, 
+			&invoice.RetailerStateID, &invoice.WholesellerID, &invoice.WholesellerName, 
+			&invoice.WholesellerEmail, &invoice.WholesellerPhone, &invoice.WholesellerAddress, 
+			&invoice.WholesellerLocationID, &invoice.WholesellerStateID, 
+			&invoice.TotalAmount, &invoice.DiscountAmount, &invoiceDate, 
+			&dueDate, &invoice.PayMode, &invoice.PayModeName, 
+			&invoice.PayType, &invoice.PayTypeName, &invoice.FinalAmount, 
+			&product.OrderItemID, &product.ProductID, &product.ProductName, 
+			&invoice.StatusName, &invoice.WholesellerStateName, &invoice.RetailerStateName, 
+			&invoice.WholesellerLocationName, &invoice.RetailerLocationName,
 		)
 
 		if err != nil {
