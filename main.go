446c602bd7ba25ee91f4c575bf479e8917cbe702
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2025-02-28 20:39:36 +0530

    Updated _test file

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
-
 	"github.com/PragaL15/go_newBackend/go_backend/db"
 	"github.com/PragaL15/go_newBackend/routes"
 	"github.com/gofiber/fiber/v2"
@@ -41,13 +40,10 @@ func main() {
 	logger, logFile := createLogger()
 	defer logFile.Close()
 	logger.Println("Server is starting...")
-
 	db.ConnectDB()
 	defer db.CloseDB()
 	app := fiber.New()
-
 	routes.RegisterRoutes(app)
-
 	go func() {
 		logger.Printf("Server is running on port %s", port)
 		if err := app.Listen(":" + port); err != nil {
