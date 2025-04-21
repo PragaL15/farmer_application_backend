bcbff9619b6641b0ac6f484d30515a0682d32c04
Pragalya Kanakaraj <pragalyakanakaraj@gmail.com>
2025-04-21 11:18:59 +0530

    Utils file is updated

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,23 @@
 package utils
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+var Logger *log.Logger
+
+func InitLogger() *os.File {
+	currentDate := time.Now().Format("2006-01-02")
+	logFileName := fmt.Sprintf("logs/%s.log", currentDate)
+	if err := os.MkdirAll("logs", os.ModePerm); err != nil {
+		log.Fatalf("Error creating logs directory: %v", err)
+	}
+	logFile, err := os.OpenFile(logFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalf("Error opening log file: %v", err)
+	}
+	Logger = log.New(logFile, "API: ", log.Ldate|log.Ltime|log.Lshortfile)
+	return logFile
+}
