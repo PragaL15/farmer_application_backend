34b7a7fb02df55e85506af96f4acb78c1184571b
Princy01 <98577352+Princy01@users.noreply.github.com>
2025-05-02 11:20:37 +0530

    Add route for super categories.go

--- routes/routes.go
+++ routes/routes.go
@@ -17,6 +17,7 @@ func RegisterRoutes(app *fiber.App) {
 	// Registering routes for GET methods
 	app.Get("/getUsers", Masterhandlers.GetAllUsers)
 	app.Get("/getCategoriesBySupID/:super_cat_id", Masterhandlers.GetCategoriesBySuperCatID)
+	app.Get("/getSuperCategories", Masterhandlers.GetSuperCategories)
 	app.Get("/getDrivers", Masterhandlers.GetDrivers)
 	app.Get("/getLocations", Masterhandlers.GetLocations)
 	app.Get("/getMandiDetails", Masterhandlers.GetAllMandiDetails)
