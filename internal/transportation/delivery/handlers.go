588f27838348aa615303131c4aefd39c791ea6d3
Dheeraj Raina <dheerajraina55@gmail.com>
2025-06-07 10:26:49 +0530

    code modification + added handler for delivery history and confirm delivery

--- internal/transportation/delivery/handlers.go
+++ internal/transportation/delivery/handlers.go
@@ -16,7 +16,9 @@ func NewDeliveryHandler(service DeliveryServiceInterface) *DeliveryHandler {
 
 func (handler *DeliveryHandler) GetActiveDeliveries(c *fiber.Ctx) error {
 
-	deliveries, err := handler.service.GetActiveDeliveries()
+	/*TODO:-  get transporterId from request header and pass it down right to the repo level  */
+	transporterId := 201
+	deliveries, err := handler.service.GetActiveDeliveries(transporterId)
 	if err != nil {
 		log.Println("Handler error:", err)
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -30,7 +32,9 @@ func (handler *DeliveryHandler) GetActiveDeliveries(c *fiber.Ctx) error {
 
 func (handler *DeliveryHandler) GetUpcomingDeliveries(c *fiber.Ctx) error {
 
-	deliveries, err := handler.service.GetUpcomingDeliveries()
+	/*TODO:-  get transporterId from request header and pass it down right to the repo level  */
+	transporterId := 201
+	deliveries, err := handler.service.GetUpcomingDeliveries(transporterId)
 	if err != nil {
 		log.Println("Handler error:", err)
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -43,8 +47,9 @@ func (handler *DeliveryHandler) GetUpcomingDeliveries(c *fiber.Ctx) error {
 }
 
 func (handler *DeliveryHandler) GetCompletedDeliveries(c *fiber.Ctx) error {
-
-	deliveries, err := handler.service.GetCompletedDeliveries()
+	/*TODO:-  get transporterId from request header and pass it down right to the repo level  */
+	transporterId := 201
+	deliveries, err := handler.service.GetCompletedDeliveries(transporterId)
 	if err != nil {
 		log.Println("Handler error:", err)
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -53,5 +58,36 @@ func (handler *DeliveryHandler) GetCompletedDeliveries(c *fiber.Ctx) error {
 	}
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"deliveries": deliveries,
+	})
+}
+
+func (handler *DeliveryHandler) GetDeliveryHistory(c *fiber.Ctx) error {
+	/*TODO:-  get transporterId from request header and pass it down right to the repo level  */
+	transporterId := 201
+	deliveries, err := handler.service.GetDeliveryHistory(transporterId)
+	if err != nil {
+		log.Println("Handler error:", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Could not fetch delivery history. Please try again later.",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"deliveries": deliveries,
+	})
+}
+
+func (handler *DeliveryHandler) ConfirmDelivery(c *fiber.Ctx) error {
+
+	enteredOtp := 9329 /*can be encrypted?*/
+	err := handler.service.ConfirmDelivery(enteredOtp)
+	// err can be request format error, wrong otp error, db error
+	if err != nil {
+		log.Println("Handler error:", err)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Could not fetch completed deliveries. Please try again later.",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Delivery Confirmed",
 	})
 }
