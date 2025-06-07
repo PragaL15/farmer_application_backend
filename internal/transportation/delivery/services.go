e3114f59b8ae3a00e1ee98bb95305929912a9890
Dheeraj Raina <dheerajraina55@gmail.com>
2025-06-07 10:25:55 +0530

    code modification + added service for delivery history

--- internal/transportation/delivery/services.go
+++ internal/transportation/delivery/services.go
@@ -3,10 +3,10 @@ package delivery
 import "fmt"
 
 type DeliveryServiceInterface interface {
-	GetActiveDeliveries() ([]Delivery, error)
-	GetUpcomingDeliveries() ([]Delivery, error)
-	GetCompletedDeliveries() ([]Delivery, error)
-	GetDeliveryHistory() ([]string, error)
+	GetActiveDeliveries(transporterId int) ([]Delivery, error)
+	GetUpcomingDeliveries(transporterId int) ([]Delivery, error)
+	GetCompletedDeliveries(transporterId int) ([]Delivery, error)
+	GetDeliveryHistory(transporterId int) ([]Delivery, error)
 	ConfirmDelivery(otp int) error
 }
 
@@ -14,18 +14,22 @@ type DeliveryService struct {
 	repository DeliveryRepositoryInterface
 }
 
-func (service *DeliveryService) GetActiveDeliveries() ([]Delivery, error) {
+func NewDeliveryService(repository DeliveryRepositoryInterface) *DeliveryService {
+	return &DeliveryService{repository: repository}
+}
 
-	deliveries, err := service.repository.GetDeliveries("active")
+func (service *DeliveryService) GetActiveDeliveries(transporterId int) ([]Delivery, error) {
+
+	deliveries, err := service.repository.GetDeliveries("active", transporterId)
 	if err != nil {
 		return nil, fmt.Errorf("service failed to get active deliveries: %w", err)
 	}
 	return deliveries, nil
 }
 
-func (service *DeliveryService) GetUpcomingDeliveries() ([]Delivery, error) {
+func (service *DeliveryService) GetUpcomingDeliveries(transporterId int) ([]Delivery, error) {
 
-	deliveries, err := service.repository.GetDeliveries("upcoming")
+	deliveries, err := service.repository.GetDeliveries("upcoming", transporterId)
 	if err != nil {
 		return nil, fmt.Errorf("service failed to get upcoming deliveries: %w", err)
 	}
@@ -33,12 +37,25 @@ func (service *DeliveryService) GetUpcomingDeliveries() ([]Delivery, error) {
 
 }
 
-func (service *DeliveryService) GetCompletedDeliveries() ([]Delivery, error) {
+func (service *DeliveryService) GetCompletedDeliveries(transporterId int) ([]Delivery, error) {
 
-	deliveries, err := service.repository.GetDeliveries("completed")
+	deliveries, err := service.repository.GetDeliveries("completed", transporterId)
 	if err != nil {
 		return nil, fmt.Errorf("service failed to get completed deliveries: %w", err)
 	}
 	return deliveries, nil
 
+}
+
+func (service *DeliveryService) GetDeliveryHistory(transporterId int) ([]Delivery, error) {
+	deliveries, err := service.repository.GetDeliveries("history", transporterId)
+	if err != nil {
+		return nil, fmt.Errorf("service failed to get delivery history: %w", err)
+	}
+	return deliveries, nil
+}
+
+func (service *DeliveryService) ConfirmDelivery(otp int) error {
+	// OTP based confirmation ?
+	return nil
 }
